06eef2c32aaef809b87676a9c3c3cb1a3a28ea36
Hamid Reza Khavarie <hr.khavarie@gmail.com>
2023-07-18 00:17:30 +0330

    final

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -57,7 +57,7 @@ const CarDetails = ({isOpen , closeModal , car}:CarDetailsProps  ) => {
 
                     <div className="flex-1 flex flex-col gap-3 ">
                         <div className="relative w-full h-32 bg-pattern bg-cover bg-center rounded-lg">
-                        <Image src={generateCarImageUrl(car  , '23')}  alt="car model1" fill priority className="object-contain"/>
+                        <Image src={generateCarImageUrl(car  , '33')}  alt="car model3" fill priority className="object-contain"/>
                         </div>
                         <div className="flex gap-3">
                             <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
