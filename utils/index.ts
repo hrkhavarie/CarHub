a93e1e0583c7cf70c370dd2c4f42b33138db9621
Hamid Reza Khavarie <hr.khavarie@gmail.com>
2023-07-18 00:14:11 +0330

    fdsf

--- utils/index.ts
+++ utils/index.ts
@@ -44,7 +44,7 @@ import { CarProps } from "@/types";
 
  }
 
- export const generateCarImageUrl = (car:CarProps  , angle?: string) => {
+ export const generateCarImageUrl = (car:CarProps  , angle: string) => {
    const url = new URL('https://cdn.imagin.studio/getimage');
 
    const {make , model , year} = car;
